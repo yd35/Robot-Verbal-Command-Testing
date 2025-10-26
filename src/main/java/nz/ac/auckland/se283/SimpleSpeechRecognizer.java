4d49e58f7fe1d93acb1eff6376f51a532d28a0ae
Aditeya Anand <aana050@aucklanduni.ac.nz>
2025-10-26 21:30:12 +1300

    Finished task 1

--- src/main/java/nz/ac/auckland/se283/SimpleSpeechRecognizer.java
+++ src/main/java/nz/ac/auckland/se283/SimpleSpeechRecognizer.java
@@ -2,10 +2,14 @@ package nz.ac.auckland.se283;
 
 public class SimpleSpeechRecognizer implements SpeechRecognizer {
 
+  private static final String DEFAULT_RESPONSE_IN_ENGLISH = "hello";
+  private static final String MAORI = "Maori";
+
   @Override
   public String recognize(String language) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'recognize'");
+    if (MAORI.equals(language)) {
+      return "kia ora";
+    }
+    return DEFAULT_RESPONSE_IN_ENGLISH; // default English
   }
-  
 }
