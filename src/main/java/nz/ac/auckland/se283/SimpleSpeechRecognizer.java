32e69aa088f2d328d5a15a5db8c9dbd12cb50b1d
Aditeya Anand <aana050@aucklanduni.ac.nz>
2025-10-26 21:22:16 +1300

    Added new class SimpleSpeechRecognizer that implements SpeechRecognizer interface and added minimum implementation for compilation method recognize()

---
+++ src/main/java/nz/ac/auckland/se283/SimpleSpeechRecognizer.java
@@ -0,0 +1,11 @@
+package nz.ac.auckland.se283;
+
+public class SimpleSpeechRecognizer implements SpeechRecognizer {
+
+  @Override
+  public String recognize(String language) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'recognize'");
+  }
+  
+}
